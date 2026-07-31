fd61735c1e10841a5f63404a43a085b5b72df46f
gurori <nazarovslava124@gmail.com>
2026-07-31 09:20:38 +0300

    refactor: add AsNoTracking and collection expression

--- DataAccess/Repositories/RoleRepository.cs
+++ DataAccess/Repositories/RoleRepository.cs
@@ -1,21 +1,21 @@
 using Application.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-namespace DataAccess.Repositories
+namespace DataAccess.Repositories;
+
+public sealed class RoleRepository(AppDbContext context) : IRoleRepository
 {
-    public sealed class RoleRepository(AppDbContext context) : IRoleRepository
+    private readonly AppDbContext _context = context;
+
+    public async Task<HashSet<int>> GetPermissionsIdsAsync(string roleName)
     {
-        private readonly AppDbContext _context = context;
+        var permissions = await _context
+            .Roles.AsNoTracking()
+            .Include(r => r.Permissions)
+            .Where(r => r.Name.ToLower() == roleName.ToLower())
+            .Select(r => r.Permissions)
+            .ToArrayAsync();
 
-        public async Task<HashSet<int>> GetPermissionsIdsAsync(string roleName)
-        {
-            var permissions = await _context
-                .Roles.Include(r => r.Permissions)
-                .Where(r => r.Name.ToLower() == roleName.ToLower())
-                .Select(r => r.Permissions)
-                .ToArrayAsync();
-
-            return permissions.SelectMany(p => p).Select(p => p.Id).ToHashSet();
-        }
+        return [.. permissions.SelectMany(p => p).Select(p => p.Id)];
     }
 }
